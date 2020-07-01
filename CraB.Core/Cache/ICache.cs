413010ba6f5ddeb33472b50ec5bdfd42e2d7e4ca
Craiqser <craiqser@gmail.com>
2020-07-01 23:23:05 +0300

    Реализация кэша в памяти.

---
+++ CraB.Core/Cache/ICache.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CraB.Core.Cache
+{
+	/// <summary>Интерфейс доступа к кэшу.</summary>
+	/// <typeparam name="T"></typeparam>
+	public interface ICache<T>
+	{
+		/// <summary>Получает значение или создаёт новое.</summary>
+		/// <param name="key">Ключ.</param>
+		/// <param name="valueNew">Функция создания кэшируемого значения для данного ключа, если оно ранее отсутствовало.</param>
+		public T Value(object key, Func<T> valueNew);
+
+		/// <summary>Асинхронно получает значение или создаёт новое.</summary>
+		/// <param name="key">Ключ.</param>
+		/// <param name="valueNew">Функция асинхронного создания кэшируемого значения для данного ключа, если оно ранее отсутствовало.</param>
+		public Task<T> Value(object key, Func<Task<T>> valueNew);
+	}
+}
---
+++ CraB.Core/Helpers/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CraB.Core
+{
+	public static class Generator
+	{
+		/// <summary>Генерирует строку заданной длины из <see cref="Guid"/>.</summary>
+		/// <param name="length">Длина строки (максимум 32 символа).</param>
+		public static string StringFromGuid(int length = 32)
+		{
+			if (length > 32)
+			{
+				throw new NotImplementedException($"Генерация строк длиной более 32 символов не реализована. Требуемая длина строки в символах: {length}.");
+			}
+
+			byte[] guid = Guid.NewGuid().ToByteArray();
+
+			return BitConverter.ToString(guid).Replace("-", string.Empty, Invariant.Comparison).Substring(0, length);
+		}
+	}
+}
